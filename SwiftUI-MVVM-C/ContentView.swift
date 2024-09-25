9f28e3d1a661325ae17ef82ca296597418bbe77b
Giovani Bettoni <bettoni.gn@gmail.com>
2024-09-25 18:56:28 -0300

    Initial Commit

---
+++ SwiftUI-MVVM-C/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-MVVM-C
+//
+//  Created by Giovani Bettoni on 25/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-MVVM-C/SwiftUI_MVVM_CApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVM_CApp.swift
+//  SwiftUI-MVVM-C
+//
+//  Created by Giovani Bettoni on 25/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVM_CApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
